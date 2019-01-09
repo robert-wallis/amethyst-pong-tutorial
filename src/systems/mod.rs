92312f6258124bb02400cc322caca94c721abbb4
Robert Wallis <smilingrob@gmail.com>
2019-01-08 23:25:40 -0800

    Paddle now uses Time

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,7 +1,7 @@
-mod bounce;
 mod ball;
+mod bounce;
 mod paddle;
 
-pub use self::bounce::BounceBallSystem;
 pub use self::ball::BallMoveSystem;
+pub use self::bounce::BounceBallSystem;
 pub use self::paddle::PaddleSystem;
--- src/systems/paddle.rs
+++ src/systems/paddle.rs
@@ -1,5 +1,5 @@
 use crate::components::{Paddle, Side};
-use amethyst::core::{nalgebra::*, Transform};
+use amethyst::core::{nalgebra::*, timing::Time, Transform};
 use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
 use amethyst::input::InputHandler;
 
@@ -10,16 +10,18 @@ impl<'s> System<'s> for PaddleSystem {
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Paddle>,
         Read<'s, InputHandler<String, String>>,
+        Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, paddles, input): Self::SystemData) {
+    fn run(&mut self, (mut transforms, paddles, input, time): Self::SystemData) {
         for (paddle, transform) in (&paddles, &mut transforms).join() {
             let movement = match paddle.side {
                 Side::Left => input.axis_value("left_paddle"),
                 Side::Right => input.axis_value("right_paddle"),
             };
             if let Some(mv_amount) = movement {
-                let y = transform.translation().y + (1.2 * mv_amount as f32);
+                let y =
+                    transform.translation().y + (time.delta_seconds() * 50.0 * mv_amount as f32);
                 let y = clamp(y, 6.0, 100.0 - 6.0);
                 transform.set_y(y);
             }
